816441b1680d5db064140bb32f6ce9f7870a6c38
SLMT <sam123456777@gmail.com>
2020-11-21 15:28:36 +0800

    Add Rust implementation

---
+++ rust/interchange-loop/src/main.rs
@@ -0,0 +1,33 @@
+use std::time::Instant;
+use rand::prelude::*;
+
+const N: usize = 1024;
+
+fn main() {
+    let mut A: Vec<Vec<f64>> = vec![vec![0.0; N]; N];
+    let mut B: Vec<Vec<f64>> = vec![vec![0.0; N]; N];
+    let mut C: Vec<Vec<f64>> = vec![vec![0.0; N]; N];
+
+    // Generate matrices
+    for i in 0 .. N {
+        for j in 0 .. N {
+            A[i][j] = rand::random::<f64>();
+            B[i][j] = rand::random::<f64>();
+        }
+    }
+
+    let start_time = Instant::now();
+
+    // Matrix Multiplication
+    for i in 0 .. N {
+        // Optimization: change loop order (j, k)
+        for k in 0 .. N {
+            for j in 0 .. N {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+
+    let elapsed = start_time.elapsed().as_secs_f64();
+    println!("Execution time: {:.6} seconds", elapsed);
+}
